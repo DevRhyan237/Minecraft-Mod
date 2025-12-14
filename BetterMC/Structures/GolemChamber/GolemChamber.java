6bc846aab3e63a7dae63799e961d816acbfdb202
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-14 17:05:37 -0300

    BetterMC

---
+++ BetterMC/Structures/GolemChamber/GolemChamber.java
@@ -0,0 +1,14 @@
+{
+  "structures": [
+    {
+      "structure": "betterminecraft:golemchamber",
+      "weight": 1
+    }
+  ],
+  "placement": {
+    "type": "minecraft:random_spread",
+    "spacing": 50,
+    "separation": 30,
+    "salt": 462654112
+  }
+}
