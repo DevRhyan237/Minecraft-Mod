5da5f59587bb737fd6dc80ee9d6160eb3fd87297
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-14 16:56:45 -0300

    BetterMC

---
+++ BetterMC/Skylands/TopazGem.java
@@ -0,0 +1,7 @@
+package net.mcreator.betterminecraft.item;
+
+public class TopazgemItem extends Item {
+	public TopazgemItem(Item.Properties properties) {
+		super(properties.rarity(Rarity.UNCOMMON));
+	}
+}
