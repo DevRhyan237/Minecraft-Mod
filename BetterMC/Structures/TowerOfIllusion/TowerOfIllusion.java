b280904033c37639dd0ebb5b4eb9f1b6b3b338ce
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-14 17:02:34 -0300

    B

---
+++ BetterMC/Structures/TowerOfIllusion/TowerOfIllusion.java
@@ -0,0 +1,27 @@
+{
+  "type": "minecraft:jigsaw",
+  "start_pool": "betterminecraft:towerof_illusion",
+  "size": 1,
+  "max_distance_from_center": 64,
+  "spawn_overrides": {},
+  "step": "surface_structures",
+  "terrain_adaptation": "none",
+  "start_height": {
+    "type": "minecraft:uniform",
+    "min_inclusive": {
+      "absolute": 60
+    },
+    "max_inclusive": {
+      "absolute": 256
+    }
+  },
+  "biomes": [
+    "snowy_slopes",
+    "jagged_peaks",
+    "frozen_peaks",
+    "betterminecraft:seasonal_biome",
+    "betterminecraft:skylands_forest",
+    "betterminecraft:skylands_plains"
+  ],
+  "use_expansion_hack": false
+}
