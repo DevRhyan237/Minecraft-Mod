74ab60ddc4a9965833d3f3b68fa7b0581c3e2770
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-14 16:57:50 -0300

    BetterMC

---
+++ BetterMC/Structures/ResStation.java
@@ -0,0 +1,14 @@
+{
+  "structures": [
+    {
+      "structure": "betterminecraft:res_station",
+      "weight": 1
+    }
+  ],
+  "placement": {
+    "type": "minecraft:random_spread",
+    "spacing": 40,
+    "separation": 20,
+    "salt": 1122165328
+  }
+}
