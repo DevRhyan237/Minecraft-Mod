8fc80cc6768402e7819613d700a4a4e6272ed21a
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 14:27:02 -0300

    BetterMC

---
+++ BetterMC/BetterEnvironment/SeasonalLog.java
@@ -0,0 +1,46 @@
+package net.mcreator.betterminecraft.block;
+
+public class SeasonalLogBlock extends Block implements SimpleWaterloggedBlock {
+	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+	public SeasonalLogBlock(BlockBehaviour.Properties properties) {
+		super(properties.sound(SoundType.CHERRY_WOOD).strength(2f, 3f).instrument(NoteBlockInstrument.BASS));
+		this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y).setValue(WATERLOGGED, false));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state) {
+		return 15;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(AXIS, WATERLOGGED);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
+		return super.getStateForPlacement(context).setValue(AXIS, context.getClickedFace().getAxis()).setValue(WATERLOGGED, flag);
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return RotatedPillarBlock.rotatePillar(state, rot);
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState updateShape(BlockState state, LevelReader world, ScheduledTickAccess scheduledTickAccess, BlockPos currentPos, Direction facing, BlockPos facingPos, BlockState facingState, RandomSource random) {
+		if (state.getValue(WATERLOGGED)) {
+			scheduledTickAccess.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+		}
+		return super.updateShape(state, world, scheduledTickAccess, currentPos, facing, facingPos, facingState, random);
+	}
+}
