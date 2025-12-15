f8e04ad261fb9b48e9de857d27a693abe69fb4f0
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 12:29:44 -0300

    BetterMC

---
+++ BetterMC/OasisFeatures/PalmLog.java
@@ -0,0 +1,73 @@
+package net.mcreator.betterminecraft.block;
+
+/* imports omitted */
+
+public class PalmLogBlock extends Block implements SimpleWaterloggedBlock {
+	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+	public PalmLogBlock(BlockBehaviour.Properties properties) {
+		super(properties.sound(SoundType.BAMBOO_WOOD).strength(2f, 10f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false).ignitedByLava().instrument(NoteBlockInstrument.BASS));
+		this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y).setValue(WATERLOGGED, false));
+	}
+
+	@Override
+	public boolean propagatesSkylightDown(BlockState state) {
+		return state.getFluidState().isEmpty();
+	}
+
+	@Override
+	public int getLightBlock(BlockState state) {
+		return 0;
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(AXIS, WATERLOGGED);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
+		return super.getStateForPlacement(context).setValue(AXIS, context.getClickedFace().getAxis()).setValue(WATERLOGGED, flag);
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return RotatedPillarBlock.rotatePillar(state, rot);
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState updateShape(BlockState state, LevelReader world, ScheduledTickAccess scheduledTickAccess, BlockPos currentPos, Direction facing, BlockPos facingPos, BlockState facingState, RandomSource random) {
+		if (state.getValue(WATERLOGGED)) {
+			scheduledTickAccess.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+		}
+		return super.updateShape(state, world, scheduledTickAccess, currentPos, facing, facingPos, facingState, random);
+	}
+
+	@Override
+	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 5;
+	}
+
+	@Override
+	public TriState canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction direction, BlockState plant) {
+		return TriState.TRUE;
+	}
+
+	@Override
+	public void onProjectileHit(Level world, BlockState blockstate, BlockHitResult hit, Projectile entity) {
+		PalmLogOnBlockHitByProjectileProcedure.execute(world, hit.getBlockPos().getX(), hit.getBlockPos().getY(), hit.getBlockPos().getZ(), hit.getDirection());
+	}
+}
