8d7f0267de0cd35a83960a2b5a2fb9beb580d8d7
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 13:46:42 -0300

    BetterMC

---
+++ BetterMC/EndFeatures/EndGravititeGem.java
@@ -0,0 +1,7 @@
+package net.mcreator.betterminecraft.item;
+
+public class EndGravititeItemItem extends Item {
+	public EndGravititeItemItem(Item.Properties properties) {
+		super(properties.rarity(Rarity.EPIC));
+	}
+}
