5c00bc1f440c9d9e9a8dde4d0e78ce592613ee3c
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 13:36:16 -0300

    BetterMC

---
+++ BetterMC/GlowingMushroomCaveFeatures/GlowingMushroomCave.java
@@ -0,0 +1,108 @@
+{
+  "has_precipitation": true,
+  "temperature": 0.4,
+  "downfall": 0.5,
+  "effects": {
+    "mood_sound": {
+      "sound": "betterminecraft:distorted_glowing",
+      "tick_delay": 1000,
+      "offset": 8,
+      "block_search_extent": 2
+    },
+    "particle": {
+      "options": {
+        "type": "betterminecraft:glowing_mushroom_particle"
+      },
+      "probability": 0.005
+    },
+    "foliage_color": -13395457,
+    "grass_color": -13421569,
+    "sky_color": -6710785,
+    "fog_color": -3355393,
+    "water_color": -16776961,
+    "water_fog_color": -16776961
+  },
+  "spawners": {
+    "monster": [
+      {
+        "type": "enderman",
+        "weight": 5,
+        "minCount": 1,
+        "maxCount": 4
+      }
+    ],
+    "creature": [],
+    "ambient": [
+      {
+        "type": "betterminecraft:glowing_mooshroom",
+        "weight": 1,
+        "minCount": 1,
+        "maxCount": 4
+      },
+      {
+        "type": "bat",
+        "weight": 10,
+        "minCount": 1,
+        "maxCount": 3
+      }
+    ],
+    "axolotls": [],
+    "underground_water_creature": [
+      {
+        "type": "glow_squid",
+        "weight": 12,
+        "minCount": 4,
+        "maxCount": 6
+      }
+    ],
+    "water_creature": [],
+    "water_ambient": [],
+    "misc": []
+  },
+  "spawn_costs": {},
+  "carvers": [
+    "minecraft:cave",
+    "minecraft:cave_extra_underground",
+    "minecraft:canyon"
+  ],
+  "features": [
+    [],
+    [
+      "minecraft:lake_lava_underground"
+    ],
+    [
+      "minecraft:amethyst_geode"
+    ],
+    [
+      "minecraft:monster_room",
+      "minecraft:monster_room_deep"
+    ],
+    [],
+    [],
+    [
+      "minecraft:ore_coal_upper",
+      "minecraft:ore_coal_lower",
+      "minecraft:ore_iron_upper",
+      "minecraft:ore_iron_middle",
+      "minecraft:ore_iron_small",
+      "minecraft:ore_gold",
+      "minecraft:ore_gold_lower",
+      "minecraft:ore_redstone",
+      "minecraft:ore_redstone_lower",
+      "minecraft:ore_diamond",
+      "minecraft:ore_diamond_medium",
+      "minecraft:ore_diamond_large",
+      "minecraft:ore_diamond_buried",
+      "minecraft:ore_lapis",
+      "minecraft:ore_lapis_buried",
+      "minecraft:ore_copper",
+      "minecraft:underwater_magma"
+    ],
+    [],
+    [],
+    [
+      "betterminecraft:glowing_mushroom_cave_tree"
+    ],
+    []
+  ]
+}
