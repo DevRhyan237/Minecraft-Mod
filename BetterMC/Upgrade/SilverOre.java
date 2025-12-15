5b012b9057cc63ad831f3d8024a2db0904fd5126
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 12:33:15 -0300

    BetterMC

--- BetterMC/Upgrade/SilverOre.java
+++ BetterMC/Upgrade/SilverOre.java
@@ -1,7 +1,5 @@
 package net.mcreator.betterminecraft.block;
 
-/* imports omitted */
-
 public class SilverOreBlock extends Block {
 	public SilverOreBlock(BlockBehaviour.Properties properties) {
 		super(properties.sound(SoundType.HEAVY_CORE).strength(4f, 3f).requiresCorrectToolForDrops());
