78937b21663b90546a9048f7de7e0331caa4d18e
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 13:51:41 -0300

    BetterMC

---
+++ BetterMC/Skylands/BlueBerryBush.java
@@ -0,0 +1,29 @@
+package net.mcreator.betterminecraft.block;
+
+public class BlueberrybushBlock extends FlowerBlock {
+	public BlueberrybushBlock(BlockBehaviour.Properties properties) {
+		super(MobEffects.MOVEMENT_SPEED, 100, properties.mapColor(MapColor.PLANT).sound(SoundType.PINK_PETALS).instabreak().noCollission().offsetType(BlockBehaviour.OffsetType.XZ).pushReaction(PushReaction.DESTROY));
+	}
+
+	@Override
+	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 100;
+	}
+
+	@Override
+	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 60;
+	}
+
+	@Override
+	public boolean mayPlaceOn(BlockState groundState, BlockGetter worldIn, BlockPos pos) {
+		return groundState.is(BetterminecraftModBlocks.SKYGRASS.get());
+	}
+
+	@Override
+	public boolean canSurvive(BlockState blockstate, LevelReader worldIn, BlockPos pos) {
+		BlockPos blockpos = pos.below();
+		BlockState groundState = worldIn.getBlockState(blockpos);
+		return this.mayPlaceOn(groundState, worldIn, blockpos);
+	}
+}
