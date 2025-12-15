2f672907695c0d85eaf365cd211962b51ce1802b
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 14:19:16 -0300

    BetterMC

---
+++ BetterMC/BetterEnvironment/SnowyGrass.java
@@ -0,0 +1,29 @@
+package net.mcreator.betterminecraft.block;
+
+public class SnowyGrassBlock extends FlowerBlock {
+	public SnowyGrassBlock(BlockBehaviour.Properties properties) {
+		super(MobEffects.DIG_SPEED, 100, properties.mapColor(MapColor.PLANT).sound(SoundType.GRASS).instabreak().noCollission().offsetType(BlockBehaviour.OffsetType.XZ).pushReaction(PushReaction.DESTROY));
+	}
+
+	@Override
+	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 100;
+	}
+
+	@Override
+	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 60;
+	}
+
+	@Override
+	public boolean mayPlaceOn(BlockState groundState, BlockGetter worldIn, BlockPos pos) {
+		return groundState.is(Blocks.GRASS_BLOCK);
+	}
+
+	@Override
+	public boolean canSurvive(BlockState blockstate, LevelReader worldIn, BlockPos pos) {
+		BlockPos blockpos = pos.below();
+		BlockState groundState = worldIn.getBlockState(blockpos);
+		return this.mayPlaceOn(groundState, worldIn, blockpos);
+	}
+}
