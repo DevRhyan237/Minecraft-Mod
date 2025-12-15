56e657a5a7bba0f8d49549b31bafdb54de84f2dc
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 13:34:22 -0300

    BetterMC

---
+++ BetterMC/OasisFeatures/Coconut.java
@@ -0,0 +1,105 @@
+package net.mcreator.betterminecraft.entity;
+
+@OnlyIn(value = Dist.CLIENT, _interface = ItemSupplier.class)
+public class CoconutEntity extends AbstractArrow implements ItemSupplier {
+	public static final ItemStack PROJECTILE_ITEM = new ItemStack(BetterminecraftModItems.THROWABLECOCONUT.get());
+	private int knockback = 0;
+
+	public CoconutEntity(EntityType<? extends CoconutEntity> type, Level world) {
+		super(type, world);
+	}
+
+	public CoconutEntity(EntityType<? extends CoconutEntity> type, double x, double y, double z, Level world, @Nullable ItemStack firedFromWeapon) {
+		super(type, x, y, z, world, PROJECTILE_ITEM, firedFromWeapon);
+		if (firedFromWeapon != null)
+			setKnockback(EnchantmentHelper.getItemEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.KNOCKBACK), firedFromWeapon));
+	}
+
+	public CoconutEntity(EntityType<? extends CoconutEntity> type, LivingEntity entity, Level world, @Nullable ItemStack firedFromWeapon) {
+		super(type, entity, world, PROJECTILE_ITEM, firedFromWeapon);
+		if (firedFromWeapon != null)
+			setKnockback(EnchantmentHelper.getItemEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.KNOCKBACK), firedFromWeapon));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public ItemStack getItem() {
+		return PROJECTILE_ITEM;
+	}
+
+	@Override
+	protected ItemStack getDefaultPickupItem() {
+		return new ItemStack(BetterminecraftModItems.THROWABLECOCONUT.get());
+	}
+
+	@Override
+	protected void doPostHurtEffects(LivingEntity entity) {
+		super.doPostHurtEffects(entity);
+		entity.setArrowCount(entity.getArrowCount() - 1);
+	}
+
+	public void setKnockback(int knockback) {
+		this.knockback = knockback;
+	}
+
+	@Override
+	protected void doKnockback(LivingEntity livingEntity, DamageSource damageSource) {
+		if (knockback > 0.0) {
+			double d1 = Math.max(0.0, 1.0 - livingEntity.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
+			Vec3 vec3 = this.getDeltaMovement().multiply(1.0, 0.0, 1.0).normalize().scale(knockback * 0.6 * d1);
+			if (vec3.lengthSqr() > 0.0) {
+				livingEntity.push(vec3.x, 0.1, vec3.z);
+			}
+		} else { // knockback might be set by firedFromWeapon passed into constructor
+			super.doKnockback(livingEntity, damageSource);
+		}
+	}
+
+	@Override
+	public void onHitBlock(BlockHitResult blockHitResult) {
+		super.onHitBlock(blockHitResult);
+		CoconutProjectileHitsBlockProcedure.execute(this.level(), blockHitResult.getBlockPos().getX(), blockHitResult.getBlockPos().getY(), blockHitResult.getBlockPos().getZ());
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		if (this.isInGround())
+			this.discard();
+	}
+
+	public static CoconutEntity shoot(Level world, LivingEntity entity, RandomSource source) {
+		return shoot(world, entity, source, 0.5f, 1, 2);
+	}
+
+	public static CoconutEntity shoot(Level world, LivingEntity entity, RandomSource source, float pullingPower) {
+		return shoot(world, entity, source, pullingPower * 0.5f, 1, 2);
+	}
+
+	public static CoconutEntity shoot(Level world, LivingEntity entity, RandomSource random, float power, double damage, int knockback) {
+		CoconutEntity entityarrow = new CoconutEntity(BetterminecraftModEntities.COCONUT.get(), entity, world, null);
+		entityarrow.shoot(entity.getViewVector(1).x, entity.getViewVector(1).y, entity.getViewVector(1).z, power * 2, 0);
+		entityarrow.setSilent(true);
+		entityarrow.setCritArrow(false);
+		entityarrow.setBaseDamage(damage);
+		entityarrow.setKnockback(knockback);
+		world.addFreshEntity(entityarrow);
+		world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.snowball.throw")), SoundSource.PLAYERS, 1, 1f / (random.nextFloat() * 0.5f + 1) + (power / 2));
+		return entityarrow;
+	}
+
+	public static CoconutEntity shoot(LivingEntity entity, LivingEntity target) {
+		CoconutEntity entityarrow = new CoconutEntity(BetterminecraftModEntities.COCONUT.get(), entity, entity.level(), null);
+		double dx = target.getX() - entity.getX();
+		double dy = target.getY() + target.getEyeHeight() - 1.1;
+		double dz = target.getZ() - entity.getZ();
+		entityarrow.shoot(dx, dy - entityarrow.getY() + Math.hypot(dx, dz) * 0.2F, dz, 0.5f * 2, 12.0F);
+		entityarrow.setSilent(true);
+		entityarrow.setBaseDamage(1);
+		entityarrow.setKnockback(2);
+		entityarrow.setCritArrow(false);
+		entity.level().addFreshEntity(entityarrow);
+		entity.level().playSound(null, entity.getX(), entity.getY(), entity.getZ(), BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.snowball.throw")), SoundSource.PLAYERS, 1, 1f / (RandomSource.create().nextFloat() * 0.5f + 1));
+		return entityarrow;
+	}
+}
