2a8fe096ee995db1207595e23d478dcea9b49477
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2025-12-15 14:21:29 -0300

    BetterMC

---
+++ BetterMC/BetterEnvironment/IceCave.java
@@ -0,0 +1,93 @@
+{
+  "has_precipitation": true,
+  "temperature": 0,
+  "downfall": 0.5,
+  "effects": {
+    "music": [
+      {
+        "weight": 1,
+        "data": {
+          "sound": "music.overworld.frozen_peaks",
+          "min_delay": 12000,
+          "max_delay": 24000,
+          "replace_current_music": true
+        }
+      }
+    ],
+    "foliage_color": -10027162,
+    "grass_color": -10027162,
+    "sky_color": -6684673,
+    "fog_color": -6684673,
+    "water_color": -10027009,
+    "water_fog_color": -10027009
+  },
+  "spawners": {
+    "monster": [
+      {
+        "type": "stray",
+        "weight": 20,
+        "minCount": 2,
+        "maxCount": 4
+      },
+      {
+        "type": "zombie",
+        "weight": 20,
+        "minCount": 2,
+        "maxCount": 4
+      },
+      {
+        "type": "spider",
+        "weight": 20,
+        "minCount": 2,
+        "maxCount": 4
+      },
+      {
+        "type": "skeleton",
+        "weight": 20,
+        "minCount": 2,
+        "maxCount": 4
+      },
+      {
+        "type": "creeper",
+        "weight": 20,
+        "minCount": 2,
+        "maxCount": 4
+      },
+      {
+        "type": "enderman",
+        "weight": 20,
+        "minCount": 1,
+        "maxCount": 2
+      }
+    ],
+    "creature": [],
+    "ambient": [
+      {
+        "type": "bat",
+        "weight": 10,
+        "minCount": 1,
+        "maxCount": 3
+      }
+    ],
+    "axolotls": [],
+    "underground_water_creature": [],
+    "water_creature": [],
+    "water_ambient": [],
+    "misc": []
+  },
+  "spawn_costs": {},
+  "carvers": [],
+  "features": [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ]
+}
