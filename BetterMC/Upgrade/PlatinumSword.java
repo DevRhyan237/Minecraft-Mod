89b2beb739cb22e6eed62d377e40764b67b47685
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-01-04 20:48:30 -0300

    BetterMC

--- BetterMC/Upgrade/PlatinumSword.java
+++ BetterMC/Upgrade/PlatinumSword.java
@@ -4,6 +4,6 @@ public class PlatinumSwordItem extends SwordItem {
 	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_WOODEN_TOOL, 750, 2.5f, 0, 10, TagKey.create(Registries.ITEM, ResourceLocation.parse("betterminecraft:platinum_sword_repair_items")));
 
 	public PlatinumSwordItem(Item.Properties properties) {
-		super(TOOL_MATERIAL, 5.5f, -2f, properties);
+		super(TOOL_MATERIAL, 5.5f, -1.75f, properties);
 	}
 }
