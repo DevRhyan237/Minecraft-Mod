e24af274a3276240490c72f47a9c9b1d1d3dd8da
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-01-04 12:17:55 -0300

    BetterMC

---
+++ BetterMC/Challenges/MiniGolem.java
@@ -0,0 +1,162 @@
+package net.mcreator.betterminecraft.entity;
+
+public class MiniGolemEntity extends Monster implements VibrationSystem {
+
+	public final AnimationState animationState0 = new AnimationState();
+
+	private final DynamicGameEventListener<VibrationSystem.Listener> dynamicGameEventListener = new DynamicGameEventListener(new VibrationSystem.Listener(this));
+	private final VibrationSystem.User vibrationUser = new VibrationUser();
+	private VibrationSystem.Data vibrationData = new VibrationSystem.Data();
+
+	public MiniGolemEntity(EntityType<MiniGolemEntity> type, Level world) {
+		super(type, world);
+		xpReward = 4;
+		setNoAi(false);
+
+	}
+
+	@Override
+	protected void registerGoals() {
+		super.registerGoals();
+
+		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal(this, Player.class, false, false));
+		this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.2, false) {
+
+			@Override
+			protected boolean canPerformAttack(LivingEntity entity) {
+				return this.isTimeToAttack() && this.mob.distanceToSqr(entity) < (this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth()) && this.mob.getSensing().hasLineOfSight(entity);
+			}
+
+		});
+		this.goalSelector.addGoal(3, new RandomStrollGoal(this, 1));
+		this.targetSelector.addGoal(4, new HurtByTargetGoal(this));
+		this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(6, new FloatGoal(this));
+
+	}
+
+	protected void dropCustomDeathLoot(ServerLevel serverLevel, DamageSource source, boolean recentlyHitIn) {
+		super.dropCustomDeathLoot(serverLevel, source, recentlyHitIn);
+		this.spawnAtLocation(serverLevel, new ItemStack(Blocks.INFESTED_STONE_BRICKS));
+	}
+
+	@Override
+	public SoundEvent getAmbientSound() {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("item.wolf_armor.break"));
+	}
+
+	@Override
+	public void playStepSound(BlockPos pos, BlockState blockIn) {
+		this.playSound(BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("block.tuff_bricks.step")), 0.15f, 1);
+	}
+
+	@Override
+	public SoundEvent getHurtSound(DamageSource ds) {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.iron_golem.damage"));
+	}
+
+	@Override
+	public SoundEvent getDeathSound() {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.iron_golem.repair"));
+	}
+
+	@Override
+	public void addAdditionalSaveData(CompoundTag compound) {
+		super.addAdditionalSaveData(compound);
+		VibrationSystem.Data.CODEC.encodeStart(this.registryAccess().createSerializationContext(NbtOps.INSTANCE), this.vibrationData).resultOrPartial(e -> BetterminecraftMod.LOGGER.error("Failed to encode vibration listener for MiniGolem: '{}'", e))
+				.ifPresent(listener -> compound.put("listener", listener));
+	}
+
+	@Override
+	public void readAdditionalSaveData(CompoundTag compound) {
+		super.readAdditionalSaveData(compound);
+		if (compound.contains("listener", Tag.TAG_COMPOUND)) {
+			VibrationSystem.Data.CODEC.parse(this.registryAccess().createSerializationContext(NbtOps.INSTANCE), compound.getCompound("listener"))
+					.resultOrPartial(e -> BetterminecraftMod.LOGGER.error("Failed to parse vibration listener for MiniGolem: '{}'", e)).ifPresent(data -> this.vibrationData = data);
+		}
+	}
+
+	@Override
+	public void updateDynamicGameEventListener(BiConsumer<DynamicGameEventListener<?>, ServerLevel> listenerConsumer) {
+		if (this.level() instanceof ServerLevel serverLevel) {
+			listenerConsumer.accept(this.dynamicGameEventListener, serverLevel);
+		}
+	}
+
+	@Override
+	public VibrationSystem.Data getVibrationData() {
+		return this.vibrationData;
+	}
+
+	@Override
+	public VibrationSystem.User getVibrationUser() {
+		return this.vibrationUser;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+
+		if (this.level() instanceof ServerLevel serverLevel) {
+			VibrationSystem.Ticker.tick(serverLevel, this.vibrationData, this.vibrationUser);
+		}
+
+		if (this.level().isClientSide()) {
+			this.animationState0.animateWhen(true, this.tickCount);
+		}
+	}
+
+	public static void init(RegisterSpawnPlacementsEvent event) {
+		event.register(BetterminecraftModEntities.MINI_GOLEM.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)),
+				RegisterSpawnPlacementsEvent.Operation.REPLACE);
+	}
+
+	public static AttributeSupplier.Builder createAttributes() {
+		AttributeSupplier.Builder builder = Mob.createMobAttributes();
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.2);
+		builder = builder.add(Attributes.MAX_HEALTH, 20);
+		builder = builder.add(Attributes.ARMOR, 0);
+		builder = builder.add(Attributes.ATTACK_DAMAGE, 5);
+		builder = builder.add(Attributes.FOLLOW_RANGE, 32);
+
+		builder = builder.add(Attributes.STEP_HEIGHT, 0.6);
+
+		builder = builder.add(Attributes.KNOCKBACK_RESISTANCE, 0.25);
+
+		builder = builder.add(Attributes.ATTACK_KNOCKBACK, 1.5);
+
+		return builder;
+	}
+
+	private class VibrationUser implements VibrationSystem.User {
+
+		private final MiniGolemEntity entity = MiniGolemEntity.this;
+		private final PositionSource positionSource = new EntityPositionSource(this.entity, this.entity.getEyeHeight());
+
+		@Override
+		public PositionSource getPositionSource() {
+			return this.positionSource;
+		}
+
+		@Override
+		public TagKey<GameEvent> getListenableEvents() {
+			return TagKey.create(Registries.GAME_EVENT, ResourceLocation.withDefaultNamespace("mini_golem_can_listen"));
+		}
+
+		@Override
+		public int getListenerRadius() {
+			return 25;
+		}
+
+		@Override
+		public boolean canReceiveVibration(ServerLevel world, BlockPos vibrationPos, Holder<GameEvent> holder, GameEvent.Context context) {
+			return true;
+		}
+
+		@Override
+		public void onReceiveVibration(ServerLevel world, BlockPos vibrationPos, Holder<GameEvent> holder, @Nullable Entity vibrationSource, @Nullable Entity projectileShooter, float distance) {
+		}
+	}
+
+}
