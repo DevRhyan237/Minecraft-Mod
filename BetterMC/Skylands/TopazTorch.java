fcc6e94f5c308c6ecc05d2fc211f919c0ff92f37
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-01-05 13:26:14 -0300

    BetterMC

--- BetterMC/Skylands/TopazTorch.java
+++ BetterMC/Skylands/TopazTorch.java
@@ -1,12 +1,8 @@
 package net.mcreator.betterminecraft.block;
 
 public class TopazTorchBlock extends Block {
-	public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
-	public static final EnumProperty<AttachFace> FACE = FaceAttachedHorizontalDirectionalBlock.FACE;
-
 	public TopazTorchBlock(BlockBehaviour.Properties properties) {
-		super(properties.sound(SoundType.BAMBOO).strength(0.1f, 0.25f).lightLevel(s -> 15).noOcclusion().isRedstoneConductor((bs, br, bp) -> false).dynamicShape().offsetType(Block.OffsetType.XZ));
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(FACE, AttachFace.WALL));
+		super(properties.sound(SoundType.BAMBOO).strength(0.1f, 0.25f).lightLevel(s -> 15).noCollission().noOcclusion().isRedstoneConductor((bs, br, bp) -> false).offsetType(Block.OffsetType.XZ));
 	}
 
 	@Override
@@ -32,49 +28,13 @@ public class TopazTorchBlock extends Block {
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
 		Vec3 offset = state.getOffset(pos);
-		return (switch (state.getValue(FACING)) {
-			default -> switch (state.getValue(FACE)) {
-				case FLOOR -> box(4, 0, 4, 16, 12, 16);
-				case WALL -> box(4, 0, 0, 16, 12, 12);
-				case CEILING -> box(0, 4, 4, 12, 16, 16);
-			};
-			case NORTH -> switch (state.getValue(FACE)) {
-				case FLOOR -> box(0, 0, 0, 12, 12, 12);
-				case WALL -> box(0, 0, 4, 12, 12, 16);
-				case CEILING -> box(4, 4, 0, 16, 16, 12);
-			};
-			case EAST -> switch (state.getValue(FACE)) {
-				case FLOOR -> box(4, 0, 0, 16, 12, 12);
-				case WALL -> box(0, 0, 0, 12, 12, 12);
-				case CEILING -> box(4, 4, 4, 16, 16, 16);
-			};
-			case WEST -> switch (state.getValue(FACE)) {
-				case FLOOR -> box(0, 0, 4, 12, 12, 16);
-				case WALL -> box(4, 0, 4, 16, 12, 16);
-				case CEILING -> box(0, 4, 0, 12, 16, 12);
-			};
-		}).move(offset.x, offset.y, offset.z);
+		return box(0, 0, 0, 12, 12, 12).move(offset.x, offset.y, offset.z);
 	}
 
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		super.createBlockStateDefinition(builder);
-		builder.add(FACING, FACE);
-	}
-
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		if (context.getClickedFace().getAxis() == Direction.Axis.Y)
-			return super.getStateForPlacement(context).setValue(FACE, context.getClickedFace().getOpposite() == Direction.UP ? AttachFace.CEILING : AttachFace.FLOOR).setValue(FACING, context.getHorizontalDirection());
-		return super.getStateForPlacement(context).setValue(FACE, AttachFace.WALL).setValue(FACING, context.getClickedFace());
-	}
-
-	public BlockState rotate(BlockState state, Rotation rot) {
-		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
-	}
-
-	public BlockState mirror(BlockState state, Mirror mirrorIn) {
-		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+	public void tick(BlockState blockstate, ServerLevel world, BlockPos pos, RandomSource random) {
+		super.tick(blockstate, world, pos, random);
+		TopazTorchOnTickUpdateProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
 	}
 
 	@Override
