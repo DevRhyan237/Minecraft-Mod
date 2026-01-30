ebbc69db85ab0d4730d90cfff5db2c279555b12f
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-01-29 22:35:28 -0300

    BetterMC

---
+++ BetterMC/Structures/AncientTomb.java
@@ -0,0 +1,14 @@
+{
+  "structures": [
+    {
+      "structure": "betterminecraft:ancient_tomb",
+      "weight": 1
+    }
+  ],
+  "placement": {
+    "type": "minecraft:random_spread",
+    "spacing": 25,
+    "separation": 15,
+    "salt": 1617407468
+  }
+}
