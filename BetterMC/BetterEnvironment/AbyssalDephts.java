296d37b4ac49e83bf7969ecd3c459767fc5e32a2
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-01-29 22:31:26 -0300

    BetterMC

---
+++ BetterMC/BetterEnvironment/AbyssalDephts.java
@@ -0,0 +1,74 @@
+{
+  "has_precipitation": true,
+  "temperature": 0.5,
+  "downfall": 0.5,
+  "effects": {
+    "mood_sound": {
+      "sound": "ambient.underwater.loop.additions.ultra_rare",
+      "tick_delay": 1000,
+      "offset": 8,
+      "block_search_extent": 2
+    },
+    "additions_sound": {
+      "sound": "ambient.underwater.loop.additions.rare",
+      "tick_chance": 0.0111
+    },
+    "music": [
+      {
+        "weight": 1,
+        "data": {
+          "sound": "music.under_water",
+          "min_delay": 12000,
+          "max_delay": 24000,
+          "replace_current_music": true
+        }
+      }
+    ],
+    "particle": {
+      "options": {
+        "type": "glow"
+      },
+      "probability": 0.005
+    },
+    "foliage_color": -16777114,
+    "grass_color": -16777114,
+    "sky_color": -16777114,
+    "fog_color": -16777114,
+    "water_color": -16777114,
+    "water_fog_color": -16777114
+  },
+  "spawners": {
+    "monster": [],
+    "creature": [],
+    "ambient": [],
+    "axolotls": [],
+    "underground_water_creature": [
+      {
+        "type": "glow_squid",
+        "weight": 10,
+        "minCount": 1,
+        "maxCount": 2
+      }
+    ],
+    "water_creature": [],
+    "water_ambient": [],
+    "misc": []
+  },
+  "spawn_costs": {},
+  "carvers": [],
+  "features": [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [
+      "betterminecraft:abyssal_depths_tree"
+    ],
+    []
+  ]
+}
