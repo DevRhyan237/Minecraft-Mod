e1bb582754721b80c215e4134c2dd08afc39e5d2
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-01-29 22:34:56 -0300

    BetterMC

---
+++ BetterMC/Structures/AbandonedCabin.java
@@ -0,0 +1,20 @@
+{
+  "type": "minecraft:jigsaw",
+  "start_pool": "betterminecraft:abandoned_cabin",
+  "size": 1,
+  "max_distance_from_center": 64,
+  "spawn_overrides": {},
+  "step": "underground_structures",
+  "terrain_adaptation": "bury",
+  "start_height": {
+    "type": "minecraft:uniform",
+    "min_inclusive": {
+      "absolute": -64
+    },
+    "max_inclusive": {
+      "absolute": 0
+    }
+  },
+  "biomes": "deep_dark",
+  "use_expansion_hack": false
+}
