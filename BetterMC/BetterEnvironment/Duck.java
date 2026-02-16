8898c35a7e763fe9c6a493aa0020a5eeeef6a5a6
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-16 20:55:49 -0300

    BetterMC

---
+++ BetterMC/BetterEnvironment/Duck.java
@@ -0,0 +1,90 @@
+package net.mcreator.betterminecraft.entity;
+
+public class DuckEntity extends Animal {
+
+	public DuckEntity(EntityType<DuckEntity> type, Level world) {
+		super(type, world);
+		xpReward = 0;
+		setNoAi(false);
+
+		setPersistenceRequired();
+
+	}
+
+	@Override
+	protected void registerGoals() {
+		super.registerGoals();
+
+		this.goalSelector.addGoal(1, new RandomStrollGoal(this, 1));
+		this.goalSelector.addGoal(2, new BreedGoal(this, 1));
+		this.goalSelector.addGoal(3, new PanicGoal(this, 1.2));
+		this.goalSelector.addGoal(4, new FollowParentGoal(this, 0.8));
+		this.goalSelector.addGoal(5, new TemptGoal(this, 1, Ingredient.of(BetterminecraftModBlocks.CATTAIL.get().asItem()), false));
+		this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(7, new FloatGoal(this));
+
+	}
+
+	@Override
+	public boolean removeWhenFarAway(double distanceToClosestPlayer) {
+		return false;
+	}
+
+	protected void dropCustomDeathLoot(ServerLevel serverLevel, DamageSource source, boolean recentlyHitIn) {
+		super.dropCustomDeathLoot(serverLevel, source, recentlyHitIn);
+		this.spawnAtLocation(serverLevel, new ItemStack(Items.FEATHER));
+	}
+
+	@Override
+	public SoundEvent getAmbientSound() {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("betterminecraft:duck_idle"));
+	}
+
+	@Override
+	public void playStepSound(BlockPos pos, BlockState blockIn) {
+		this.playSound(BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.chicken.step")), 0.15f, 1);
+	}
+
+	@Override
+	public SoundEvent getHurtSound(DamageSource ds) {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.chicken.hurt"));
+	}
+
+	@Override
+	public SoundEvent getDeathSound() {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.chicken.death"));
+	}
+
+	@Override
+	public AgeableMob getBreedOffspring(ServerLevel serverWorld, AgeableMob ageable) {
+		DuckEntity retval = BetterminecraftModEntities.DUCK.get().create(serverWorld, EntitySpawnReason.BREEDING);
+		retval.finalizeSpawn(serverWorld, serverWorld.getCurrentDifficultyAt(retval.blockPosition()), EntitySpawnReason.BREEDING, null);
+		return retval;
+	}
+
+	@Override
+	public boolean isFood(ItemStack stack) {
+		return Ingredient.of(BetterminecraftModBlocks.CATTAIL.get().asItem()).test(stack);
+	}
+
+	public static void init(RegisterSpawnPlacementsEvent event) {
+		event.register(BetterminecraftModEntities.DUCK.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+				(entityType, world, reason, pos, random) -> (world.getBlockState(pos.below()).is(BlockTags.ANIMALS_SPAWNABLE_ON) && world.getRawBrightness(pos, 0) > 8), RegisterSpawnPlacementsEvent.Operation.REPLACE);
+	}
+
+	public static AttributeSupplier.Builder createAttributes() {
+		AttributeSupplier.Builder builder = Mob.createMobAttributes();
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
+		builder = builder.add(Attributes.MAX_HEALTH, 10);
+		builder = builder.add(Attributes.ARMOR, 0);
+		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
+		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
+
+		builder = builder.add(Attributes.STEP_HEIGHT, 0.6);
+
+		builder = builder.add(Attributes.TEMPT_RANGE, 10);
+
+		return builder;
+	}
+
+}
