5bdd60969d542bcbbf2cbf792b2f4b4d73f81a20
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-16 20:57:40 -0300

    BetterMC

---
+++ BetterMC/Structures/BattleDungeon/DungeonBricks.java
@@ -0,0 +1,40 @@
+package net.mcreator.betterminecraft.block;
+
+public class DungeonBricksBlock extends Block implements SimpleWaterloggedBlock {
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+	public DungeonBricksBlock(BlockBehaviour.Properties properties) {
+		super(properties.sound(SoundType.NETHER_BRICKS).strength(3.5f, 9f).requiresCorrectToolForDrops());
+		this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state) {
+		return 15;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(WATERLOGGED);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
+		return super.getStateForPlacement(context).setValue(WATERLOGGED, flag);
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState updateShape(BlockState state, LevelReader world, ScheduledTickAccess scheduledTickAccess, BlockPos currentPos, Direction facing, BlockPos facingPos, BlockState facingState, RandomSource random) {
+		if (state.getValue(WATERLOGGED)) {
+			scheduledTickAccess.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+		}
+		return super.updateShape(state, world, scheduledTickAccess, currentPos, facing, facingPos, facingState, random);
+	}
+}
