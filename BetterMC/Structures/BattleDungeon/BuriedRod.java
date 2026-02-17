bcb220423399a50d4ea5e3925c7d100016d648d0
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-16 21:01:51 -0300

    BetterMC

---
+++ BetterMC/Structures/BattleDungeon/BuriedRod.java
@@ -0,0 +1,7 @@
+package net.mcreator.betterminecraft.item;
+
+public class BuriedRodItem extends Item {
+	public BuriedRodItem(Item.Properties properties) {
+		super(properties.rarity(Rarity.UNCOMMON));
+	}
+}
