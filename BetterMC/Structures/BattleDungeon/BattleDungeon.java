ddf0ec30dcdddb214a6ea3f139ca9995f6f7d53b
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-16 21:01:26 -0300

    BetterMC

---
+++ BetterMC/Structures/BattleDungeon/BattleDungeon.java
@@ -0,0 +1,54 @@
+{
+  "type": "minecraft:jigsaw",
+  "start_pool": "betterminecraft:battle_dungeon",
+  "size": 1,
+  "max_distance_from_center": 64,
+  "spawn_overrides": {},
+  "step": "strongholds",
+  "terrain_adaptation": "bury",
+  "start_height": {
+    "type": "minecraft:uniform",
+    "min_inclusive": {
+      "absolute": 0
+    },
+    "max_inclusive": {
+      "absolute": 50
+    }
+  },
+  "biomes": [
+    "plains",
+    "forest",
+    "savanna",
+    "taiga",
+    "jungle",
+    "meadow",
+    "dark_forest",
+    "desert",
+    "cherry_grove",
+    "swamp",
+    "betterminecraft:rainforest",
+    "betterminecraft:tundra",
+    "betterminecraft:seasonal_biome",
+    "betterminecraft:desert_cave",
+    "betterminecraft:ice_cave",
+    "betterminecraft:steppe",
+    "betterminecraft:glowing_mushroom_cave",
+    "betterminecraft:oasis",
+    "ice_spikes",
+    "sunflower_plains",
+    "snowy_taiga",
+    "sparse_jungle",
+    "flower_forest",
+    "badlands",
+    "eroded_badlands",
+    "bamboo_jungle",
+    "wooded_badlands",
+    "windswept_forest",
+    "windswept_gravelly_hills",
+    "windswept_hills",
+    "windswept_savanna",
+    "lush_caves",
+    "dripstone_caves"
+  ],
+  "use_expansion_hack": false
+}
