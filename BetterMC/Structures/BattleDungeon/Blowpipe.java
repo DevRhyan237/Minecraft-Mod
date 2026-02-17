7cf6f3647b5b8643adea20c1a95331755915d331
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-16 21:02:13 -0300

    BetterMC

---
+++ BetterMC/Structures/BattleDungeon/Blowpipe.java
@@ -0,0 +1,62 @@
+package net.mcreator.betterminecraft.item;
+
+public class BlowpipeItem extends Item {
+	public BlowpipeItem(Item.Properties properties) {
+		super(properties.rarity(Rarity.RARE).durability(64));
+	}
+
+	@Override
+	public ItemUseAnimation getUseAnimation(ItemStack itemstack) {
+		return ItemUseAnimation.BLOCK;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack, LivingEntity livingEntity) {
+		return 122000;
+	}
+
+	@Override
+	public InteractionResult use(Level world, Player entity, InteractionHand hand) {
+		InteractionResult ar = InteractionResult.FAIL;
+		if (entity.getAbilities().instabuild || findAmmo(entity) != ItemStack.EMPTY) {
+			ar = InteractionResult.SUCCESS;
+			entity.startUsingItem(hand);
+		}
+		return ar;
+	}
+
+	@Override
+	public boolean releaseUsing(ItemStack itemstack, Level world, LivingEntity entity, int time) {
+		if (!world.isClientSide() && entity instanceof ServerPlayer player) {
+			ItemStack stack = findAmmo(player);
+			if (player.getAbilities().instabuild || stack != ItemStack.EMPTY) {
+				ItemStack arrowPickupStack = stack;
+				if (arrowPickupStack.isEmpty()) {
+					arrowPickupStack = new ItemStack(Items.ARROW);
+					arrowPickupStack.set(DataComponents.INTANGIBLE_PROJECTILE, Unit.INSTANCE);
+				}
+				Arrow projectile = new Arrow(world, entity, arrowPickupStack, itemstack);
+				projectile.shootFromRotation(entity, entity.getXRot(), entity.getYRot(), 0, 3.15f, 1.0F);
+				world.addFreshEntity(projectile);
+				world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.arrow.shoot")), SoundSource.PLAYERS, 1, 1f / (world.getRandom().nextFloat() * 0.5f + 1));
+				itemstack.hurtAndBreak(1, entity, LivingEntity.getSlotForHand(entity.getUsedItemHand()));
+				if (player.getAbilities().instabuild) {
+					projectile.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+				} else {
+					if (stack.isDamageableItem()) {
+						if (world instanceof ServerLevel serverLevel)
+							stack.hurtAndBreak(1, serverLevel, player, _stkprov -> {
+							});
+					} else {
+						stack.shrink(1);
+					}
+				}
+			}
+		}
+		return super.releaseUsing(itemstack, world, entity, time);
+	}
+
+	private ItemStack findAmmo(Player player) {
+		return new ItemStack(Items.ARROW);
+	}
+}
