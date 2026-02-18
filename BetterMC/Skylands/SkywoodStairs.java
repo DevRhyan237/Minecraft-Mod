6575d65be37fa9cac82123571ffbb8b1e717fc44
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-18 14:53:52 -0300

    BetterMC

---
+++ BetterMC/Skylands/SkywoodStairs.java
@@ -0,0 +1,27 @@
+package net.mcreator.betterminecraft.block;
+
+public class SkywoodpackStairsBlock extends StairBlock {
+	public SkywoodpackStairsBlock(BlockBehaviour.Properties properties) {
+		super(Blocks.AIR.defaultBlockState(), properties.sound(SoundType.CHERRY_WOOD).strength(2f, 3f).ignitedByLava().instrument(NoteBlockInstrument.BASS));
+	}
+
+	@Override
+	public float getExplosionResistance() {
+		return 3f;
+	}
+
+	@Override
+	public int getLightBlock(BlockState state) {
+		return 0;
+	}
+
+	@Override
+	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 20;
+	}
+
+	@Override
+	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 5;
+	}
+}
