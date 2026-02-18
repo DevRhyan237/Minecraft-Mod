d80b7027c8d42534edf6c2fd192064ca2ab93e28
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-02-18 14:46:41 -0300

    BetterMC

---
+++ BetterMC/OasisFeatures/PalmFence.java
@@ -0,0 +1,22 @@
+package net.mcreator.betterminecraft.block;
+
+public class Palm_wood_packFenceBlock extends FenceBlock {
+	public Palm_wood_packFenceBlock(BlockBehaviour.Properties properties) {
+		super(properties.sound(SoundType.CHERRY_WOOD).strength(2f, 3f).ignitedByLava().instrument(NoteBlockInstrument.BASS).forceSolidOn());
+	}
+
+	@Override
+	public int getLightBlock(BlockState state) {
+		return 0;
+	}
+
+	@Override
+	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 20;
+	}
+
+	@Override
+	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return 5;
+	}
+}
