4a97926eb178c19e31d8d1e20adc5e770a5187e2
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-03-02 20:18:16 -0300

    BetterMC

---
+++ BetterMC/Structures/PirateShip/PirateShip.java
@@ -0,0 +1,30 @@
+{
+  "type": "minecraft:jigsaw",
+  "start_pool": "betterminecraft:pirate_ship",
+  "size": 1,
+  "max_distance_from_center": 64,
+  "spawn_overrides": {},
+  "step": "top_layer_modification",
+  "terrain_adaptation": "none",
+  "start_height": {
+    "type": "minecraft:trapezoid",
+    "min_inclusive": {
+      "absolute": 62
+    },
+    "max_inclusive": {
+      "absolute": 63
+    }
+  },
+  "biomes": [
+    "ocean",
+    "cold_ocean",
+    "frozen_ocean",
+    "deep_lukewarm_ocean",
+    "lukewarm_ocean",
+    "deep_frozen_ocean",
+    "deep_ocean",
+    "deep_cold_ocean",
+    "warm_ocean"
+  ],
+  "use_expansion_hack": false
+}
