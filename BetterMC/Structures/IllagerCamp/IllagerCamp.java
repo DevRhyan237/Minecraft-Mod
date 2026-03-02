df5185d9995ff85a027d2f8d40d2ce19c2170137
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-03-02 20:22:01 -0300

    BetterMC

---
+++ BetterMC/Structures/IllagerCamp/IllagerCamp.java
@@ -0,0 +1,14 @@
+{
+  "structures": [
+    {
+      "structure": "betterminecraft:illager_camp",
+      "weight": 1
+    }
+  ],
+  "placement": {
+    "type": "minecraft:random_spread",
+    "spacing": 25,
+    "separation": 15,
+    "salt": 1071351777
+  }
+}
